a4454f265e35edb3b36e66a84a31508f63afce30
Friedrich Beckmann <friedrich.beckmann@tha.de>
2026-03-04 23:12:06 +0100

    new upstream version 2.1.0

--- Formula/pspp.rb
+++ Formula/pspp.rb
@@ -1,8 +1,8 @@
 class Pspp < Formula
   desc "statistical analysis of sampled data"
   homepage "https://www.gnu.org/software/pspp"
-  url "https://ftpmirror.gnu.org/gnu/pspp/pspp-2.0.1.tar.gz"
-  sha256 "8edbb0f09e8cf8010cad9e0559e0230d7fc5aae4721c756c350554df33024c00"
+  url "https://ftpmirror.gnu.org/gnu/pspp/pspp-2.1.0.tar.gz"
+  sha256 "6268c2847c7d9eedbbe57801120720ad7821de8978f311b7bf5ec13b9304e710"
   head "https://benpfaff.org/~blp/pspp-master/latest-source.tar.gz"
 
   option "with-relocation", "Build a relocatable application which is required for a bundle"
